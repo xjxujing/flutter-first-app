6ada14db14a38c0f4d946afeb7a518398bbb4f65
xjxujing <xujing_xj@foxmail.com>
2020-03-24 18:11:43 +0800

    使用 List 数据，有两种写法

--- lib/main.dart
+++ lib/main.dart
@@ -8,83 +8,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int level = 1;
+//  定义一个列表
+  List<String> datas = ["hello world", "hello flutter", "hello niuniu"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("个人介绍"),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level += 1;
-          });
-        },
-        backgroundColor: Colors.grey[800],
-        child: Icon(Icons.add),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-//                backgroundImage: AssetImage("assets/images/image.jpeg"),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-            Text("姓名",
-                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
-            SizedBox(height: 10.0),
-            Text("妞妞",
-                style: TextStyle(
-                    color: Colors.amberAccent[200],
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0)),
-            SizedBox(height: 30.0),
-            Text("公司",
-                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
-            SizedBox(height: 10.0),
-            Text("阿里巴巴 新零售",
-                style: TextStyle(
-                    color: Colors.amberAccent[200],
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0)),
-            SizedBox(height: 30.0),
-            Text("等级",
-                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
-            SizedBox(height: 10.0),
-            Text("$level",
-                style: TextStyle(
-                    color: Colors.amberAccent[200],
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0)),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(Icons.email, color: Colors.grey[400]),
-                SizedBox(width: 10.0),
-                Text(
-                  "xujing_xj@foxmail.com",
-                  style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 18.0,
-                      letterSpacing: 2.0),
-                )
-              ],
-            )
-          ],
-        ),
+      body: Column(
+//        children: datas.map((data){
+//          return Text(data);
+//        }).toList(),
+        children: datas.map((data) => Text(data)).toList(),
+//        children: <Widget>[
+//          Text("hello world"),
+//          Text("hello flutter"),
+//          Text("hello niuniu"),
+//        ],
       ),
     );
   }
