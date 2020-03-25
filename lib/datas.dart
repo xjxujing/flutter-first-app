3e0921accbca475e151c1fb1dc2a003f6252fb6c
xjxujing <xujing_xj@foxmail.com>
2020-03-25 13:56:29 +0800

    重构目录结构

--- lib/datas.dart
+++
@@ -1,9 +0,0 @@
-class Datas {
-  String text;
-  String author;
-
-//  main.dart 中的封装的卡片相当于在这里定义
-//  Object data;
-
-  Datas({this.text, this.author});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,5 @@
 import 'package:flutter/material.dart';
-import 'datas.dart';
-import 'datas_card.dart';
+//import 'pages/home.dart';
+import 'package:first_app/pages/home.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<Datas> datas = [
-    Datas(text: "hello lucy", author: "lucy"),
-    Datas(text: "hello niuniu", author: "niuniu"),
-    Datas(text: "hello lufei", author: "lufei"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("个人介绍"),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-      ),
-      body: Column(
-        // 这里使用封装的 widget
-        children: datas.map((data) => DataCard(
-          data: data,
-          delete: (){
-            setState(() {
-              datas.remove(data);
-            });
-          }
-        )).toList(),
-      ),
-    );
-  }
-}
