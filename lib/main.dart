b36eb69f704564579f738ffb9cbed293f8a198e1
xjxujing <xujing_xj@foxmail.com>
2020-03-25 10:05:26 +0800

    封装卡片 Widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,29 @@ class Datas {
 
 class _HomeState extends State<Home> {
   List<Datas> datas = [
-    Datas(text: "hello", author: "lucy"),
-    Datas(text: "hello", author: "niuniu"),
-    Datas(text: "hello", author: "lufei"),
+    Datas(text: "hello lucy", author: "lucy"),
+    Datas(text: "hello niuniu", author: "niuniu"),
+    Datas(text: "hello lufei", author: "lufei"),
   ];
+
+  // 封装的 widget
+  Widget dataTemplate(data) {
+    return Card(
+        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(data.text,
+                  style: TextStyle(fontSize: 18.0, color: Colors.grey[600])),
+              SizedBox(height: 6.0),
+              Text(data.author,
+                  style: TextStyle(fontSize: 14.0, color: Colors.grey[800])),
+            ],
+          ),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,16 +50,8 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.grey[850],
       ),
       body: Column(
-//        children: datas.map((data){
-//          return Text(data);
-//        }).toList(),
-        children:
-            datas.map((data) => Text('${data.text}:${data.author}')).toList(),
-//        children: <Widget>[
-//          Text("hello world"),
-//          Text("hello flutter"),
-//          Text("hello niuniu"),
-//        ],
+        // 这里使用封装的 widget
+        children: datas.map((data) => dataTemplate(data)).toList(),
       ),
     );
   }
