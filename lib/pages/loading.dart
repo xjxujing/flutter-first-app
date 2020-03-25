6e6a05d6e9be30c38d15ccf3a2fca91a26ed3792
xjxujing <xjxujing_xj@foxmail.com>
2020-03-25 16:46:21 +0800

    获取世界时间，处理请求回来的数据

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
-
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
@@ -11,12 +10,18 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
   void getData() async {
     // http request
-    Response response = await get("https://jsonplaceholder.typicode.com/todos/1");
+    Response response = await get("http://worldclockapi.com/api/json/est/now");
     // 把 json 数据转成 map 类型
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data["title"]);
+    // print(data);
+    String currentDateTime = data["currentDateTime"];
+    String offset = data["utcOffset"].substring(1, 3);
+    // 上面获取到的当前时间和与格林威治相差的时间
 
+    // 创建时间对象，还原时间
+    DateTime now = DateTime.parse(currentDateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
