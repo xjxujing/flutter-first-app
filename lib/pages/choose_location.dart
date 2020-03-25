a9cabe180818753f53fab0117941e143e757da9f
xjxujing <xjxujing_xj@foxmail.com>
2020-03-25 15:16:17 +0800

    生命周期函数

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,16 +6,40 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("initState function ran");
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("build function ran");
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text("选择一个地区"),
-        centerTitle: true,
-        elevation: 0,
-      ),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text("选择一个地区"),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: RaisedButton(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+          child: Text("counter is $counter"),
+        ));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("dispose function ran");
   }
 }
